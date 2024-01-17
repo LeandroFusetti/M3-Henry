6d7d6934495df216aa6aece9bcfd19c6d492b69d
Leandro Fusetti <leandrofusetti@gmail.com>
2024-01-16 23:48:13 -0300

    Testing terminado

--- 07-Testing/homework/01 - Exercises/Part 01/levels.js
+++ 07-Testing/homework/01 - Exercises/Part 01/levels.js
@@ -1,9 +1,65 @@
-const levelOne = (a, b) => {};
+const levelOne = (a, b) => {
+    return a + b
+};
 
-const levelTwo = (letras) => {};
+const levelTwo = (letras) => {
+    let array=[]
+    if(letras.length<2) return letras
+    if(letras.length==2)return letras[0]
+    if(letras.length >2){
+        for(let i=0; i<=letras.length;i++){
+            if(!(i%2)){
+                array.push(letras[i])
+            }
 
-const levelThree = (a, b) => {};
+            
+        }
+        return array.join('')
 
-const levelFour = (num) => {};
+    }
 
+};
+
+
+
+const levelThree = (a, b) => {
+    let array= a.concat(b)
+    for(let i=0; i<=array.length-1; i++){
+        
+        for(let j=0; j<=array.length-1; j++){
+            if(array[j]>array[j+1]){
+                let aux= array[j]
+                array[j]= array[j+1]
+                array[j+1]=aux
+            }
+        }
+
+    }
+    return array
+
+};
+
+
+const levelFour = (num) => {
+    
+    let accu = 0
+    let rev;
+    num= num.toString()
+    
+    for(let i=0; i<=num.length-1;i++){
+        accu += Number(num[i])
+        
+    }
+    
+    rev=accu
+    if(num.length>1){
+        console.log(num);
+        rev= accu.toString().split('').reverse().join('')
+        
+    }
+    return num == (rev * accu)
+   
+};
+
+console.log(levelFour(1458))
 module.exports = { levelOne, levelTwo, levelThree, levelFour };
--- 07-Testing/homework/01 - Exercises/Part 02/antiTrollsSecurity.js
+++ 07-Testing/homework/01 - Exercises/Part 02/antiTrollsSecurity.js
@@ -1,3 +1,22 @@
-const antiTrollsSecurity = (string) => {};
+const antiTrollsSecurity = (string) => {
+
+    let array = string.split('')
+    
+    let eliminaVocales= (x)=>{
+        
+        if(x=="a"|| x=="e" || x =="i" || x =="o"|| x=="u" || x=="A"|| x=="E" || x =="I" || x =="O"|| x=="U"){
+            x=""
+            return x
+        }   else return x
+    }
+
+    let sinVoc = array.map(eliminaVocales)
+    
+      return sinVoc.join("")
+   
+
+};// tambien se puede hacer con el metodo replace()
+
 
 module.exports = antiTrollsSecurity;
+
